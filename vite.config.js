23427338dbe83d6efba67ce5893292fed9ea2f3f
sailist <sailist@outlook.com>
2023-04-17 09:54:25 +0800

    动态别名解析

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 import { defineConfig } from "vite";
+import path, { resolve } from "path";
+import { builtinEnvironments, populateGlobal } from "vitest/environments";
 
-import { builtinEnvironments, populateGlobal } from 'vitest/environments'
-
-console.log(builtinEnvironments) // { jsdom, happy-dom, node, edge-runtime }
+const root = resolve(__dirname, "src/");
+export const isDev = process.env.__DEV__ === "true";
 
 export default defineConfig({
-  // ...
+  resolve: {
+    alias: [
+      {
+        find: /^@(.+)/,
+        replacement: (match, p1) => path.resolve(__dirname, "src", p1),
+      },
+    ],
+  },
   test: {
     // environment: "happy-dom",
     environment: "jsdom",
     // environment: "edge-runtime",
-    
   },
 });
-
