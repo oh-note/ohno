d838d5b480703eab9b091dd4d0c1496beb1acc30
sailist <sailist@outlook.com>
2024-03-26 09:14:56 +0800

    prepare for publish

---
+++ DOC.md
@@ -0,0 +1,30 @@
+
+## Why Yet another Editor
+
+
+
+- Heavy initialization
+- <!-- https://necolas.github.io/normalize.css/ -->
+- <!-- css module -->
+<!-- eslint prettier -->
+
+features：
+
+- 无重复渲染，无事件处理部分的 element 和 preview 完全一致
+- 易扩展
+- 基于 contenteditable
+- Markdown-style hint + Markdown-Format
+- 行为绑定
+
+# 开发文档
+
+- [day 1: 技术选型](./solutions/day1.md)
+- [day 2: 事件系统](./solutions/day2.md)
+- [day 3: Page 和 Block 的基本设计](./solutions/day3.md)
+- [day 4: 光标移动](./solutions/day4.md)
+- [day 5: 编辑操作 & 历史记录](./solutions/day5.md)
+
+# Reference
+
+- [@floating-ui](https://github.com/floating-ui/floating-ui)
+- https://d-toybox.com/studio/lib/input_event_viewer.html
--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -34,4 +34,25 @@ export default defineConfig({
   test: {
     environment: "jsdom",
   },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ohno-core",
+      // the proper extensions will be added
+      fileName: "ohno",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      // external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        // globals: {
+        //   vue: "Vue",
+        // },
+      },
+    },
+  },
 });
